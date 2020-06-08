d8b3d60e2bd37319c60b456ab4aaa76ec3202ade
harshit860 <harshitga@gmail.com>
2020-06-09 00:44:04 +0530

    harshit

--- Client/src/components/Chat.js
+++ Client/src/components/Chat.js
@@ -19,7 +19,13 @@ const Chat = ({location}) =>{
             setName(name)
             setRoom(room)
 
-            console.log(socket)
+            socket.emit('join',{name , room})
+
+          return () =>{
+              socket.emit('disconnect');
+              
+              socket.off()
+          }
     },[ENDPOINT,location.search])
 
     return (
--- Server/index.js
+++ Server/index.js
@@ -8,10 +8,17 @@ const port = process.env.PORT || 5000
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
-
+const {addUser , removeUser,getUser,getUserInRoom }  = require('./Users')
 
 io.on('connection',(socket) =>{
     console.log("we have a new connections !!!")
+
+    socket.on('join',({name,room},callback) => {
+        console.log(name , room)
+        const {error ,user } = addUser({id:socket.id,name,room})
+
+        if(error ) return callback(error)
+    })
 
     socket.on('disconnect', () => {
         console.log(" User disconnected")
