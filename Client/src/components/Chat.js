13da3d5e71d7e62e58f52f2849d39f8514aff52c
harshit860 <harshitga@gmail.com>
2020-06-08 19:40:38 +0530

    Chat module changes'

--- Client/src/components/Chat.js
+++ Client/src/components/Chat.js
@@ -2,8 +2,26 @@ import React,{useState,useEffect} from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
+let socket;
 
-const Chat = () =>{
+const Chat = ({location}) =>{
+
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = 'localhost:5000';
+
+
+    useEffect(()=>{
+            const{name , room}  = queryString.parse(location.search)
+
+            socket = io(ENDPOINT)
+            
+            setName(name)
+            setRoom(room)
+
+            console.log(socket)
+    },[ENDPOINT,location.search])
+
     return (
         <div>Chat</div>
     )
